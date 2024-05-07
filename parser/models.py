4426710f861c0adee47ddf6e897029587393d321
user <kola89611@gmail.com>
2024-05-07 16:09:33 +0300

    Extended: model

--- parser/models.py
+++ parser/models.py
@@ -13,16 +13,25 @@ class User(db.Model, UserMixin):
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     category = db.Column(db.Integer(), db.ForeignKey('category.id'))
-    title = db.Column(db.String(length=20), nullable=False, unique=True)
     api_key = db.Column(db.String(length=30), nullable=False, unique=True)
+
+    title = db.Column(db.String(length=20), nullable=False, unique=True)
+    max_supply = db.Column(db.Numeric(scale=12))
+    date_added = db.Column(db.DateTime)
     price = db.Column(db.Numeric(scale=12))
     market_cap = db.Column(db.Numeric(scale=6))
-    change_percent = db.Column(db.Numeric(scale=2))
-    change_value = db.Column(db.Numeric(scale=6))
+    percent_change_1h = db.Column(db.Numeric(scale=2))
+    percent_change_24h = db.Column(db.Numeric(scale=2))
+    percent_change_7d = db.Column(db.Numeric(scale=2))
+    percent_change_30d = db.Column(db.Numeric(scale=2))
+    percent_change_60d = db.Column(db.Numeric(scale=2))
+    percent_change_90d = db.Column(db.Numeric(scale=2))
+    volume_24h = db.Column(db.Numeric(scale=6))
+    volume_change_24h = db.Column(db.Numeric(scale=6))
     last_updated = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __str__(self):
-        return self.key_link
+        return self.api_key
 
 
 class Category(db.Model):
