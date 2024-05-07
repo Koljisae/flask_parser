cb861e4647d56e3623f3a78d6b7b50fdb74bbcf6
user <kola89611@gmail.com>
2024-05-07 16:10:10 +0300

    Added: new fields and detail_view

--- parser/views.py
+++ parser/views.py
@@ -23,18 +23,39 @@ def get_crypto_data(api_key: str):
     try:
         response = session.get(API_CRYPTO_URL, params=parameters)
         if response.status_code == 200:
-            data = json.loads(response.text)['data'][api_key]['quote']['USD']
+            data = json.loads(response.text)['data'][api_key]
+            name = data.get('name')
+            max_supply = data.get('max_supply')
+            date_added = data.get('date_added')
+            date_added = datetime.fromisoformat(date_added.replace('Z', '+00:00'))
+
+            data = data['quote']['USD']
             price = data.get('price')
             market_cap = data.get('market_cap')
-            change_percent = data.get('percent_change_24h')
-            change_value = data.get('volume_change_24h')
+            percent_change_1h = data.get('percent_change_1h')
+            percent_change_24h = data.get('percent_change_24h')
+            percent_change_7d = data.get('percent_change_7d')
+            percent_change_30d = data.get('percent_change_30d')
+            percent_change_60d = data.get('percent_change_60d')
+            percent_change_90d = data.get('percent_change_90d')
+            volume_24h = data.get('volume_24h')
+            volume_change_24h = data.get('volume_change_24h')
             last_updated = data.get('last_updated')
             last_updated = datetime.fromisoformat(last_updated.replace('Z', '+00:00'))
             return {
+                'title': name,
+                'max_supply': max_supply,
+                'date_added': date_added,
                 'price': price,
                 'market_cap': market_cap,
-                'change_percent': change_percent,
-                'change_value': change_value,
+                'percent_change_1h': percent_change_1h,
+                'percent_change_24h': percent_change_24h,
+                'percent_change_7d': percent_change_7d,
+                'percent_change_30d': percent_change_30d,
+                'percent_change_60d': percent_change_60d,
+                'percent_change_90d': percent_change_90d,
+                'volume_24h': volume_24h,
+                'volume_change_24h': volume_change_24h,
                 'last_updated': last_updated,
             }
         else:
@@ -53,15 +74,30 @@ def home(page: int = 1):
     return render_template('home.html', currencies=currencies, title='Home')
 
 
-@views.route('/update_crypto')
+@views.route('/update', methods=['GET'])
 def update_crypto():
     currencies = Item.query.filter_by(category=2)
     for currency in currencies:
         data = get_crypto_data(currency.api_key)
+        currency.title = data.get('title')
+        currency.max_supply = data.get('max_supply')
+        currency.date_added = data.get('date_added')
         currency.price = data.get('price')
         currency.market_cap = data.get('market_cap')
-        currency.change_percent = data.get('change_percent')
-        currency.change_value = data.get('change_value')
+        currency.percent_change_1h = data.get('percent_change_1h')
+        currency.percent_change_24h = data.get('percent_change_24h')
+        currency.percent_change_7d = data.get('percent_change_7d')
+        currency.percent_change_30d = data.get('percent_change_30d')
+        currency.percent_change_60d = data.get('percent_change_60d')
+        currency.percent_change_90d = data.get('percent_change_90d')
+        currency.volume_24h = data.get('volume_24h')
+        currency.volume_change_24h = data.get('volume_change_24h')
         currency.last_updated = data.get('last_updated')
         db.session.commit()
     return redirect(url_for('views.home', title='Update crypto'))
+
+
+@views.route('/detail/<string:api_key>', methods=['GET'])
+def detail_crypto(api_key=None):
+    currency = Item.query.filter_by(api_key=api_key, category=2).first()
+    return render_template('detail.html', currency=currency, title=f'{currency.title} details')
